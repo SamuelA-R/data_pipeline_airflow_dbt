7ffd04aac24ad005b2f5680d37bb4492d72cd3d8
Samuel Alves <samuelalvesribeiro111@gmail.com>
2025-09-01 23:51:39 -0300

    apis internacionais

--- main.py
+++ main.py
@@ -7,9 +7,38 @@ from keys.api_key import get_api_key
 
 API_KEY = get_api_key()
 
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo'
+bronze = os.getenv('BRONZE', 'C:\\projetos-paralelos-samuel\\data-pipeline-dbt-airflow\\data\\bronze')
+url = 'https://www.alphavantage.co/query'
 
-r = requests.get(url, params=paramers)
-data = r.json()
+TICKERS = [
+    "AAPL", "GOOGL", "MSFT",  # Ações estrangeiras
+    "PETR4.SA", "VALE3.SA", "ITUB4.SA"  # Ações brasileiras
+]
 
-print(data)
+def fetch_stock_data(TICKERS):
+
+    for ticker in TICKERS:
+        paramers = {
+            'function': 'TIME_SERIES_DAILY',
+            'symbol': ticker,
+            'outputsize': 'compact',  # or 'full'
+            'datatype': 'json',
+            'apikey': API_KEY
+        }
+        r = requests.get(url, params=paramers)
+        data = r.json()
+
+        print(data)
+
+        answer = requests.get(url=url, params=paramers)
+
+        if r.status_code == 200:
+            dados = r.json()
+            nome_arquivo = f"{ticker}.json"
+            caminho_arquivo = os.path.join(bronze, nome_arquivo)
+            with open(caminho_arquivo, 'w') as arquivo:
+                json.dump(dados, arquivo)
+            
+            print(f"Dados ticker {ticker} salvo em {caminho_arquivo}")
+
+fetch_stock_data(TICKERS)
