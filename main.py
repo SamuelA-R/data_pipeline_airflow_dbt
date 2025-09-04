debf5149ce42ba45bcdaa2a9b77f48707dec2acf
Samuel Alves <samuelalvesribeiro111@gmail.com>
2025-09-03 23:46:51 -0300

    codigo extracao api

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 import logging
 from keys.api_key import get_api_key
 
-API_KEY = get_api_key()
+API_KEY = os.getenv("API_KEY_STOCK")
 
 bronze = os.getenv('BRONZE', 'C:\\projetos-paralelos-samuel\\data-pipeline-dbt-airflow\\data\\bronze')
 url = 'https://www.alphavantage.co/query'
@@ -13,6 +13,12 @@ url = 'https://www.alphavantage.co/query'
 TICKERS = [
     "AAPL", "GOOGL", "MSFT",  # Ações estrangeiras
     "PETR4.SA", "VALE3.SA", "ITUB4.SA"  # Ações brasileiras
+]
+
+INDICADORES = [
+    "EMA", "SMA", "CCI", "WMA", "DEMA", 
+    "TEMA", "KAMA", "ADX", "RSI", 
+    "WILLR", "OBV"
 ]
 
 def fetch_stock_data(TICKERS):
@@ -40,5 +46,56 @@ def fetch_stock_data(TICKERS):
                 json.dump(dados, arquivo)
             
             print(f"Dados ticker {ticker} salvo em {caminho_arquivo}")
+        else:
+            print(f"Erro ao buscar dados do ticker {ticker}: {r.status_code} - {r.text}")
+
+def dados_tesouro():
+    os.makedirs(bronze, exist_ok=True)
+    parameters = {
+        "function": "TREASURY_YIELD",
+        "interval": "daily",
+        "apikey": API_KEY,
+        "maturity":"2year",
+        "interval":"daily"
+    }
+
+    r = requests.get(url, params=parameters)
+    if r.status_code == 200:
+        dados = r.json()
+        nome_arquivo = f"tesouro.json"
+        caminho_arquivo = os.path.join(bronze, nome_arquivo)
+
+        with open(caminho_arquivo, 'w') as arquivo:
+            json.dump(dados, arquivo)
+
+        print(f"Dados Tesouro salvo em {caminho_arquivo}")
+    else:
+        print(f"Erro ao buscar dados do Tesouro: {r.status_code} -{r.text}")
+
+def captura_indicadores_tecnicos(INDICADORES, TICKERS):
+    for indicador, ticker in zip(INDICADORES, TICKERS):
+        parameters = {
+            "function": "TECHNICAL_INDICATOR",
+            "symbol": ticker,
+            "interval": "daily",
+            "time_period": "10",
+            "series_type": "close",
+            "apikey": API_KEY
+        }
+
+        r = requests.get(url, params=parameters)
+        if r.status_code == 200:
+            dados = r.json()
+            nome_arquivo = f"{ticker}_{indicador}.json"
+            caminho_arquivo = os.path.join(bronze, nome_arquivo)
+
+            with open(caminho_arquivo, 'w') as arquivo:
+                json.dump(dados, arquivo)
+
+            print(f"Dados {indicador} para o ticker {ticker} salvo em {caminho_arquivo}")
+        else:
+            print(f"Erro ao buscar dados do {indicador} para o ticker {ticker}: {r.status_code} - {r.text}")
 
 fetch_stock_data(TICKERS)
+dados_tesouro()
+captura_indicadores_tecnicos(INDICADORES, TICKERS)
